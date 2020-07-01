c399fff0a1c79339abde863d8ba9f29d87b59d3b
farhandrmwn <farhanbrobro@gmail.com>
2020-07-01 09:03:19 +0700

    sanber

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1><b>SELAMAT DATANG!</b></h1>
+    <h3>Terima kasih telah bergabung di SanberBook. Social Media kita bersama!</h3>
+</body>
+</html>
